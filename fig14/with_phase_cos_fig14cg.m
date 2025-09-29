af15619473f85ce917edb8d8e3426dfc87229300
HZ-Shen <shztc19871015@163.com>
2025-09-29 17:40:33 +0800

    Update with_phase_cos_fig14cg.m

--- fig14/with_phase_cos_fig14cg.m
+++ fig14/with_phase_cos_fig14cg.m
@@ -3,7 +3,7 @@ clear;
 close all;
 tic;
 
-% 程序中所提到的方程Eq .(10)和Eq .(11)为论文---AX12404孙思杰的论文250103.pdf---里的。
+% 
 
 % parameters setting
 tau_0 = 1;
@@ -35,14 +35,14 @@ t = linspace(0, tf, 1000);
 y = deval(sol, t);
 
 % Calculate the atomic excitation
-Final_x = y(1,:).*conj(y(1,:));   %Cxt的纯数值解
-Final_e = y(2,:).*conj(y(2,:));   %Cet的纯数值解
+Final_x = y(1,:).*conj(y(1,:));   %
+Final_e = y(2,:).*conj(y(2,:));   %
 
 
 
-% 渐进稳态解Cxt
+% 
 eps=zeros(size(t)); 
-omega_j = 6.19176; %对应于论文中的omega_j
+omega_j = 6.19176; %
 s_j = -1i*omega_j;
 cx0 = sqrt(0.8);
 ce0 = sqrt(0.2);
@@ -52,9 +52,9 @@ for i=1:length(t)
 end  
 Final_xeq = eps;
 
-% 渐进稳态解Cet
+% 
 eps=zeros(size(t)); 
-omega_j = 6.19176; %对应于论文中的omega_j
+omega_j = 6.19176; %
 s_j = -1i*omega_j;
 cx0 = sqrt(0.8);
 ce0 = sqrt(0.2);
@@ -99,8 +99,8 @@ toc
 
 
 %%%%%%%%%%%%%%%%%%%%%setting the deday differential equation%%%%%%%%%%%%%
-% 当前延迟微分方程对应于 ---AX12404孙思杰的论文250103.pdf--- 中的Eq. (10)和Eq. (11)，注意加入了相位
-% 如果延迟微分方程变了，只需要理解并修改这部分内容即可继续使用此程序 !!!
+% 
+% 
 function dxdtf = ddefunf(tau_0, Gamma, mu_1, mu_2, G, omega_l, N, alpha)
 
 dxdtf = @ddefun;
@@ -119,9 +119,9 @@ end
 
 for m = 2:N
     for n = 1:m-1
-        count = (m-1)*(m-2)/2;       % count 的作用是跳过当前m之前的所有已赋值的数组元素
-        x = n+count;                 % x代表了与m-n相关的延迟项在Z这个数组中的位置，这个位置(Z中的第二个指标)与相应的与m-n相关的延迟项所对应的延迟是什么有关，(N^2-N)/2是延迟项m-n(或m+n)所需要的数组大小
-        y = x+((N^2-N)/2);           % y代表了与m+n相关的延迟项在Z这个数组中的位置，这个位置(Z中的第二个指标)与相应的与m+n相关的延迟项所对应的延迟是什么有关
+        count = (m-1)*(m-2)/2;       % 
+        x = n+count;                 % 
+        y = x+((N^2-N)/2);           % 
 %         t2 = -(Gamma).*cos(alpha.*n-alpha.*m).*Z(1,x).*heaviside(t-abs(m-n).*tau_0).*exp(1i.*0.5.*omega_l.*abs(m-n).*tau_0);
 %         t4 = (Gamma).*cos(alpha.*n-alpha.*m).*Z(1,y).*heaviside(t-(m+n).*tau_0).*exp(1i.*0.5.*omega_l.*(m+n).*tau_0);
         t2 = 0;
@@ -132,7 +132,7 @@ for m = 2:N
 end
 
 for m = 1:N 
-    y1 = (N^2-N)+m;   % y1代表了与2*m相关的延迟项在Z这个数组中的位置，这个位置(Z中的第二个指标)与相应的与2*m相关的延迟项所对应的延迟是什么有关，(N^2-N)是延迟项m-n加延迟项m+n一共所需要的数组大小
+    y1 = (N^2-N)+m;   % 
     t3 = (Gamma/2).*Z(1,y1).*heaviside(t-(2*m).*tau_0).*exp(1i.*0.5.*omega_l.*(2*m).*tau_0);
     s3 = s3+t3;
 end
@@ -147,7 +147,7 @@ end
 
 
 %%%%%%%%%%%%% non constant delay%%%%%%%%%%%%%%%%%%%
-% 如果延迟微分方程变了，只需要理解并修改这部分内容即可继续使用此程序 !!!
+% 
 function delayf = ddelagsf(tau_0,N)
 
 delayf = @ddelags;
@@ -157,17 +157,17 @@ delay = zeros([1 N^2]);
 for m = 2:N
     for n = 1:m-1
         count = (m-1)*(m-2)/2;
-        x = n+count;   % 这里对应在数组Z位置x处的延迟项
-        y = x+((N^2-N)/2);           % 这里对应在数组Z位置y处的延迟项
-        delay(1,x) = t-abs(m-n)*tau_0;   % 赋值对应于数组Z在位置x处的延迟项的延迟
-        delay(1,y) = t-(m+n)*tau_0;      % 赋值对应于数组Z在位置y处的延迟项的延迟
+        x = n+count;   % 
+        y = x+((N^2-N)/2);           % 
+        delay(1,x) = t-abs(m-n)*tau_0;   % 
+        delay(1,y) = t-(m+n)*tau_0;      %
     end
 end
 
 for m = (N^2-N)+1:(N^2-N)+N
-        y1 = m;                           % 这里对应在数组Z位置y1处的延迟项
+        y1 = m;                           % 
         m1 = m-(N^2-N);
-        delay(1,y1) = t-(2*m1)*tau_0;      % 赋值对应于数组Z在位置y1处的延迟项的延迟
+        delay(1,y1) = t-(2*m1)*tau_0;      %
 end
 
 delay = delay';
@@ -175,9 +175,9 @@ end
 
 end
 
-%%%%%%%%%%%Cxt渐进稳态解%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%
 function result = cxtfun(tau0,N1,G,gamma,omegal,mu1,mu2,alpha,cx0,ce0,s)
-    x1 = (sqrt(-1)*(-1)).*ce0.*G+cx0.*(sqrt(-1).*mu2+s);                      %对应于Eq. 13的分子
+    x1 = (sqrt(-1)*(-1)).*ce0.*G+cx0.*(sqrt(-1).*mu2+s);                      %
     us1 = (1/2).*exp(1).^((-2).*s.*tau0+(-2).*N1.*s.*tau0).*((-1)+exp(1).^( ...
   2.*((sqrt(-1)*(1/2)).*omegal+(-1).*s).*tau0)).^(-1).*((-1).*exp(1) ...
   .^(sqrt(-1).*omegal.*tau0+sqrt(-1).*N1.*omegal.*tau0)+exp(1).^(2.* ...
@@ -198,7 +198,7 @@ function result = cxtfun(tau0,N1,G,gamma,omegal,mu1,mu2,alpha,cx0,ce0,s)
     result = abs(x1./us1)^2;
 end
 
-%%%%%%%%%%%Cet渐进稳态解%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%
 function result = cetfun(tau0,N1,G,gamma,omegal,mu1,mu2,alpha,cx0,ce0,s)
     x1 = (sqrt(-1)*(-1)).*cx0.*G+ce0.*((1/2).*exp(1).^((-2).*s.*tau0+(-2).* ...
   N1.*s.*tau0).*((-1)+exp(1).^(2.*((sqrt(-1)*(1/2)).*omegal+(-1).*s) ...
@@ -225,3 +225,4 @@ function result = cetfun(tau0,N1,G,gamma,omegal,mu1,mu2,alpha,cx0,ce0,s)
   2.*N1.*s.*tau0)).*gamma.*((-2).*tau0+(-2).*N1.*tau0));
     result = abs(x1./us1)^2;
 end
+
